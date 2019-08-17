21c6c4a57573017b39305e43464740045d7ef845
Maxim Puchkov <mpuchkov@sfu.ca>
2019-08-17 20:35:05 +0600

    Removed Navigation comment

--- AdFree/AllRulesViewController.swift
+++ AdFree/AllRulesViewController.swift
@@ -46,14 +46,4 @@ class AllRulesViewController: ViewController {
         self.textView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
